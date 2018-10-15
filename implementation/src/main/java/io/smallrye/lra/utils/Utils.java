019679cc73c64f551fa053ef0d04905fdd3d90b2
xstefank <xstefank122@gmail.com>
2018-10-15 20:26:04 +0200

    Clean up

--- implementation/src/main/java/io/smallrye/lra/utils/Utils.java
+++ implementation/src/main/java/io/smallrye/lra/utils/Utils.java
@@ -1,8 +1,6 @@
 package io.smallrye.lra.utils;
 
-import javax.ws.rs.core.Response;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class Utils {
@@ -14,9 +12,5 @@ public class Utils {
     public static String getFormattedString(URL parentLRA, String clientID, Long timeout, TimeUnit unit) {
         return String.format("[parentLRA = %s, clientID = %s, timeout = %s, unit = %s]",
                 parentLRA, clientID, timeout, unit);
-    }
-
-    public static boolean hasStatus(int actual, Response.Status... expected) {
-        return Arrays.stream(expected).anyMatch(i -> i.getStatusCode() == actual);
     }
 }
