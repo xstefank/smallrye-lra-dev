9a0e2d8830f6e7ed5e7b62ae171e202323aae2bc
xstefank <xstefank122@gmail.com>
2018-10-28 21:16:16 +0100

    Add scripts directory, update download script to run only when coordinator is not yet downlaoded

--- tck/scripts/download_narayana_coordinator.sh
+++ tck/scripts/download_narayana_coordinator.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cd $WORKDIR
+BASEDIR=..
+NARAYANA_VERSION=5.9.0.Final
+
+if [ -f $BASEDIR/lra-coordinator-swarm.jar ]; then
+    echo "Coordinator already downloaded at $BASEDIR/lra-coordinator-swarm.jar"
+    exit 0
+fi
+
+cd $BASEDIR
 
 wget http://www.jboss.org/jbosstm/downloads/$NARAYANA_VERSION/binary/narayana-full-$NARAYANA_VERSION-bin.zip
 
@@ -11,4 +19,5 @@ cp narayana-full-$NARAYANA_VERSION/rts/lra/lra-coordinator-swarm.jar ./
 rm -rf narayana-full-$NARAYANA_VERSION-bin.zip
 
 rm -rf narayana-full-$NARAYANA_VERSION
- 
+
+
