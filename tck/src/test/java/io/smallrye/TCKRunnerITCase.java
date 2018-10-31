d45e2ddd04b1af8b1ebcab4a2eea3e85d825b36a
xstefank <xstefank122@gmail.com>
2018-10-31 21:08:48 +0100

    Make TCK work automatically

--- tck/src/test/java/io/smallrye/TCKRunnerITCase.java
+++ tck/src/test/java/io/smallrye/TCKRunnerITCase.java
@@ -2,6 +2,11 @@ package io.smallrye;
 
 import org.junit.Test;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import java.io.File;
 
 public class TCKRunnerITCase {
@@ -10,7 +15,7 @@ public class TCKRunnerITCase {
 
     @Test
     public void runTCK() throws Exception {
-        ProcessBuilder coordinatorPb = new ProcessBuilder("java", "-jar", "lra-coordinator-swarm.jar", "-Dswarm.http.port=8082");
+        ProcessBuilder coordinatorPb = new ProcessBuilder("java", "-jar", "lra-coordinator-swarm.jar");
         coordinatorPb.inheritIO();
         coordinatorPb.directory(new File("."));
         coordinatorPb.redirectOutput(new File("target/coodinator-output.txt"));
@@ -19,8 +24,8 @@ public class TCKRunnerITCase {
         Thread.sleep(10000);
 
         ProcessBuilder TCKClientPb = new ProcessBuilder("java", "-jar", 
-                "smallrye-lra-tck-1.0-SNAPSHOT-thorntail.jar", "-Dswarm.port.offset=100",
-                "-Dio.smallrye.lra.LRACoordinatorRESTClient/mp-rest/url=http://localhost:8082");
+                "smallrye-lra-tck-1.0-SNAPSHOT-thorntail.jar", "-Dswarm.port.offset=100", 
+                "-Dio.smallrye.lra.LRACoordinatorRESTClient/mp-rest/url=http://localhost:8080");
         TCKClientPb.inheritIO();
         TCKClientPb.redirectOutput(new File("target/tck-client-output.txt"));
         TCKClientPb.directory(new File("target"));
@@ -28,6 +33,11 @@ public class TCKRunnerITCase {
         Process TCKClientProcess = TCKClientPb.start();
         Thread.sleep(10000);
 
+//        System.out.println("Executing TCK run...");
+//        WebTarget target = ClientBuilder.newClient().target("http://localhost:8180/tck/all");
+//        Response response = target.request().put(Entity.json(""));
+//        System.out.println(response.readEntity(String.class));
+        
         ProcessBuilder tckPb = new ProcessBuilder("curl", "-XPUT", "http://localhost:8180/tck/all");
         tckPb.inheritIO();
         tckPb.redirectOutput(new File("target/tck-execution-output.txt"));
