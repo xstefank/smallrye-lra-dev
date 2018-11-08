96918a751ac74b42079a9ae148fb62b8e6e3d073
xstefank <xstefank122@gmail.com>
2018-11-08 21:49:34 +0100

    Add LRA filter cases

--- implementation/src/main/java/io/smallrye/lra/LRARequestFilter.java
+++ implementation/src/main/java/io/smallrye/lra/LRARequestFilter.java
@@ -1,7 +1,9 @@
 package io.smallrye.lra;
 
 import org.eclipse.microprofile.lra.annotation.LRA;
+import org.eclipse.microprofile.lra.client.LRAClient;
 
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ResourceInfo;
@@ -14,15 +16,93 @@ import java.lang.reflect.Method;
 public class LRARequestFilter implements ContainerRequestFilter {
 
     @Context
-    protected ResourceInfo resourceInfo;
+    private ResourceInfo resourceInfo;
+    
+    @Inject
+    private LRAClient lraClient;
     
     @Override
     public void filter(ContainerRequestContext ctx) throws IOException {
         Method resourceMethod = resourceInfo.getResourceMethod();
         LRA lra = resourceMethod.getAnnotation(LRA.class);
 
-        if (lra != null) {
-            System.out.println(lra.value());
+        if (lra == null) {
+            return;
+        }
+
+        String lraHeader = ctx.getHeaderString(LRAClient.LRA_HTTP_HEADER);
+        boolean shouldeJoin = lra.join();
+
+        switch (lra.value()) {
+            /**
+             *  If called outside a LRA context a JAX-RS filter will begin a new
+             *  LRA for the duration of the method call and when the call completes
+             *  another JAX-RS filter will complete the LRA.
+             */
+            case REQUIRED:
+                if (lraHeader != null) {
+                    System.out.println(lraHeader);
+                }
+                break;
+
+                    /**
+                     *  If called outside a LRA context a JAX-RS filter will begin a new
+                     *  LRA for the duration of the method call and when the call completes
+                     *  another JAX-RS filter will complete the LRA.
+                     *
+                     *  If called inside a LRA context a JAX-RS filter will suspend it and
+                     *  begin a new LRA for the duration of the method call and when the call
+                     *  completes another JAX-RS filter will complete the LRA and resume the
+                     *  one that was active on entry to the method.
+                     */
+            case REQUIRES_NEW:
+                break;
+
+                    /**
+                     *  If called outside a transaction context, the method call will return
+                     *  with a 412 Precondition Failed HTTP status code
+                     *
+                     *  If called inside a transaction context the bean method execution will
+                     *  then continue within that context.
+                     */
+            case MANDATORY:
+                break;
+
+                    /**
+                     *  If called outside a LRA context the bean method execution
+                     *  must then continue outside a LRA context.
+                     *
+                     *  If called inside a LRA context the managed bean method execution
+                     *  must then continue inside this LRA context.
+                     */
+            case SUPPORTS:
+                break;
+
+                    /**
+                     *  The bean method is executed without a LRA context. If a context is
+                     *  present on entry then it is suspended and then resumed after the
+                     *  execution has completed.
+                     */
+            case NOT_SUPPORTED:
+                break;
+
+                    /**
+                     *  If called outside a LRA context the managed bean method execution
+                     *  must then continue outside a LRA context.
+                     *
+                     *  If called inside a LRA context the method is not executed and a
+                     *  <code>412 Precondition Failed</code> HTTP status code is returned
+                     *  to the caller.
+                     */
+            case NEVER:
+                break;
+
+        }
+
+        if (shouldeJoin) {
+//            lraClient.joinLRA(lraHeader)
         }
     }
+    
+    
 }
