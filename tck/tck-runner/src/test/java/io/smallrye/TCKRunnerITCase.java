4124b5f3c1beec786941fb3e844a49f2bc49245b
xstefank <xstefank122@gmail.com>
2018-11-22 20:55:03 +0100

    Run all tests in TCK

--- tck/tck-runner/src/test/java/io/smallrye/TCKRunnerITCase.java
+++ tck/tck-runner/src/test/java/io/smallrye/TCKRunnerITCase.java
@@ -43,7 +43,7 @@ public class TCKRunnerITCase {
         waitForTCK();
 
         System.out.println("Executing TCK run...");
-        WebTarget target = ClientBuilder.newClient().target("http://localhost:8180/tck/cancelOnFamily");
+        WebTarget target = ClientBuilder.newClient().target("http://localhost:8180/tck/all" );
         Response response = target.request().put(null);
 
         destroyProcess(coordinatorProcess);
