1687ac20c87e7c6d6bcd2db8f55f42f193fe8712
xstefank <xstefank122@gmail.com>
2018-11-25 22:59:17 +0100

    Add participant registration to LRAManagement

--- implementation/src/main/java/io/smallrye/lra/management/SmallRyeLRAManagement.java
+++ implementation/src/main/java/io/smallrye/lra/management/SmallRyeLRAManagement.java
@@ -1,5 +1,6 @@
 package io.smallrye.lra.management;
 
+import org.eclipse.microprofile.lra.client.GenericLRAException;
 import org.eclipse.microprofile.lra.client.LRAClient;
 import org.eclipse.microprofile.lra.participant.JoinLRAException;
 import org.eclipse.microprofile.lra.participant.LRAManagement;
@@ -8,6 +9,8 @@ import org.eclipse.microprofile.lra.participant.LRAParticipantDeserializer;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,16 +23,27 @@ public class SmallRyeLRAManagement implements LRAManagement {
     
     @Inject
     private LRAClient lraClient;
+
+    @Context
+    private UriInfo uriInfo;
     
     @Override
     public String joinLRA(LRAParticipant participant, URL lraId, Long timeLimit, TimeUnit unit) throws JoinLRAException {
-//        lraClient.joinLRA(lraId, )
-        return null;
+        String recoveryUrl;
+        
+        try {
+            recoveryUrl = lraClient.joinLRA(lraId, LRAParticipantResource.class, uriInfo.getBaseUri(), null);
+            participants.put(recoveryUrl, new SmallRyeLRAParticipant(participant));
+        } catch (GenericLRAException e) {
+            throw new JoinLRAException(lraId, e.getStatusCode(), e.getMessage(), e);
+        }
+        
+        return recoveryUrl;
     }
 
     @Override
     public String joinLRA(LRAParticipant participant, URL lraId) throws JoinLRAException {
-        return null;
+        return joinLRA(participant, lraId, 0L, TimeUnit.SECONDS);
     }
 
     @Override
