14540509eff943198d6703c69286057c55e73420
xstefank <xstefank122@gmail.com>
2019-02-13 22:59:56 +0100

    Fix noLRATest passing

--- tck/src/test/java/io/smallrye/lra/tck/extension/SmallRyeLRAAuxiliaryArchiveAppender.java
+++ tck/src/test/java/io/smallrye/lra/tck/extension/SmallRyeLRAAuxiliaryArchiveAppender.java
@@ -4,6 +4,7 @@ import io.smallrye.lra.SmallRyeLRAClient;
 import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
 import java.io.File;
@@ -16,7 +17,8 @@ public class SmallRyeLRAAuxiliaryArchiveAppender implements AuxiliaryArchiveAppe
                 .addPackage("org.eclipse.microprofile.lra.annotation")
                 .addPackage("org.eclipse.microprofile.lra.client")
                 .addPackage("org.eclipse.microprofile.lra.participant")
-                .addPackages(true, SmallRyeLRAClient.class.getPackage());
+                .addPackages(true, SmallRyeLRAClient.class.getPackage())
+                .addAsManifestResource(new StringAsset("io.smallrye.lra.filter.LRAClientRequestFilter"), "services/javax.ws.rs.ext.Providers");
 
         File configFile = new File(ClassLoader.getSystemResource("META-INF/microprofile-config.properties").getFile());
         lraJar.addAsManifestResource(configFile, "microprofile-config.properties");
